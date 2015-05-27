dd592c785cf34943f5dc43c31c0295a34579fec1
mhofman <mel@capone.nl>
2015-05-27 23:24:00 +0200

    Removing unused requires etc.

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
+var fs = require('fs');
 var restify = require('restify');
-var session = require('restify-session')({
-    debug: true,
-    ttl: 2
-});
-
 var server = restify.createServer({
-    name: 'ABN-Amro stub server',
+    name: 'RESTAFarian Server - converting Backbone Collections into REST endpoints',
     version: '0.0.1'
 });
 
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
-server.use(session.sessionManager);
 
 var REST = require('./REST.js');
-new REST(server, session, require('./modules/Elephants.js'), 'elephants');
-new REST(server, session, require('./modules/Zookeepers.js'), 'zookeepers');
+var collections = './collections/';
+
+fs.readdir(collections, function (err, files) {
+    if (err) {
+        throw err;
+    }
+    files.forEach(function (file) {
+        new REST(server, require(collections + file), file.replace(/\.js$/, '').toLowerCase());
+    });
+});
 
 server.listen(4242, function () {
     console.log('%s listening at %s', server.name, server.url);
