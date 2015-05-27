c3c9e4adcfd5175091fc38883b99a87f27f84b7e
mhofman <mel@capone.nl>
2015-05-27 21:42:46 +0200

    rewrite to facilitate testing

--- REST.js
+++ REST.js
@@ -1,16 +1,33 @@
+var _ = require('underscore');
 var REST = function (server, session, collection, endpoint) {
     this.server = server;
     this.session = session;
-    this.session[endpoint] = collection;
+    this.collection = collection;
     this.endpoint = endpoint;
     this.run();
 };
 
 REST.prototype = {
     create: function (req, res, next) {
-        var model = this.session[this.endpoint].add(req.body);
+        var model = this.collection.add(req.body);
         res.send(model.toJSON());
         return next();
+    },
+    getPrevNext: function (url, start, limit) {
+
+        if (!/\?/.test(url)) {
+            url += '?start=' + start + '&limit=' + limit;
+        } else if (!/start/.test(url)) {
+            url += '&start=' + start + '&limit=' + limit;
+        }
+
+        var prev = url.replace(/start=[0-9]+/, 'start=' + (start - limit));
+        var next = url.replace(/start=[0-9]+/, 'start=' + (start + limit));
+
+        return {
+            prev: prev,
+            next: next
+        }
     },
     readAll: function (req, res, done) {
 
@@ -24,34 +41,29 @@ REST.prototype = {
             limit = 1;
         }
 
-        var total = this.session[this.endpoint].size();
+        var total = this.collection.size();
 
-        if (!/\?/.test(req.url)) {
-            req.url += '?start=' + start + '&limit=' + limit;
-        }
-
-        req.url = 'http://localhost:4242' + req.url;
-
-        var prev = req.url.replace(/start=[0-9]+/, 'start=' + (start - limit));
-        var next = req.url.replace(/start=[0-9]+/, 'start=' + (start + limit));
+        var prevNext = this.getPrevNext('http://' + req.header('host', this.server.url) + req.url, start, limit);
         var sendObject = {
             total: total,
-            data: this.session[this.endpoint].toJSON().slice(start - 1, start + limit - 1)
+            data: this.collection.toJSON().slice(start - 1, start + limit - 1),
+            start: start,
+            limit: limit
         };
 
         if (start > 1) {
-            sendObject.prev = prev;
+            sendObject.prev = prevNext.prev;
         }
 
         if (start + limit <= total) {
-            sendObject.next = next;
+            sendObject.next = prevNext.next;
         }
 
         res.send(sendObject);
         return done();
     },
     readById: function (req, res, next) {
-        var model = this.session[this.endpoint].get(req.params.id);
+        var model = this.collection.get(req.params.id);
         if (model) {
             res.send(model);
         } else {
@@ -62,7 +74,7 @@ REST.prototype = {
         return next();
     },
     readFacets: function (req, res, next) {
-        var names = this.session[this.endpoint].pluck(req.params.facet);
+        var names = this.collection.pluck(req.params.facet);
         var facets = {};
         for (var i = 0; i < names.length; i++) {
             if (facets[names[i]]) {
@@ -75,7 +87,7 @@ REST.prototype = {
         return next();
     },
     update: function (req, res, next) {
-        var model = this.session[this.endpoint].get(req.params.id);
+        var model = this.collection.get(req.params.id);
         if (model) {
             model.set(req.body);
             res.send(model);
@@ -87,10 +99,10 @@ REST.prototype = {
         return next();
     },
     del: function (req, res, next) {
-        var model = this.session[this.endpoint].get(req.params.id);
+        var model = this.collection.get(req.params.id);
         if (model) {
             model = model.toJSON();
-            this.session[this.endpoint].remove(req.params.id);
+            this.collection.remove(req.params.id);
             res.send(model);
         } else {
             res.send(404, {
@@ -98,6 +110,12 @@ REST.prototype = {
             });
         }
         return next();
+    },
+    extend: function (method, route, callback) {
+        this.server[method]('/' + this.endpoint + '/' + route, function (req, res, next) {
+            callback(req, res, next);
+            return next();
+        });
     },
     run: function () {
         var that = this;
