2ea06246a958b7fb36e45205985b6de862b4ad6f
mhofman <mel@capone.nl>
2015-05-27 23:44:35 +0200

    Make collection load configurable

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var restify = require('restify');
 var server = restify.createServer({
     name: 'RESTAFarian Server - converting Backbone Collections into REST endpoints',
-    version: '0.0.1'
+    version: require('./package').version
 });
 
 server.use(restify.acceptParser(server.acceptable));
@@ -10,7 +10,11 @@ server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
 var REST = require('./REST.js');
-var collections = './collections/';
+var collections = process.env.COLLECTIONS || './collections/';
+
+if (!process.env.COLLECTIONS) {
+    console.log('Loading example collections');
+}
 
 fs.readdir(collections, function (err, files) {
     if (err) {
@@ -18,6 +22,7 @@ fs.readdir(collections, function (err, files) {
     }
     files.forEach(function (file) {
         new REST(server, require(collections + file), file.replace(/\.js$/, '').toLowerCase());
+        console.log('Added enpoint', file.replace(/\.js$/, '').toLowerCase())
     });
 });
 
