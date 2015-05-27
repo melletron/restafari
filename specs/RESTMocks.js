94b680b7fbd1e4c05821db5b2c09b39dfcaa44f5
mhofman <mel@capone.nl>
2015-05-27 23:26:04 +0200

    Storing test doubles in a seperate file

---
+++ specs/RESTMocks.js
@@ -0,0 +1,74 @@
+var sinon = require('sinon');
+module.exports = function () {
+    return {
+        server: {
+            get: sinon.spy(),
+            post: function () {
+            },
+            put: function () {
+            },
+            del: function () {
+            }
+        },
+        req: {
+            query: {
+                start: 1,
+                limit: 10
+            },
+            url: '/elephants',
+            header: function (header) {
+                switch (header) {
+                    case 'host':
+                        return 'localhost:4242';
+                        break;
+                }
+            },
+            body: 'request body',
+            params: {
+                id: 'id',
+                facet: 'colour'
+            }
+        },
+        res: {
+            send: sinon.spy()
+        },
+        collection: {
+            toJSON: function () {
+                return '1;2;3;4;5;6;7;8;9;10;11;12;13;14;15;16;17;18;19;20'.split(';');
+            },
+            size: function () {
+                return this.toJSON().length;
+            },
+            add: sinon.spy(function () {
+                return {
+                    toJSON: function () {
+                        return {
+                            'new': 'model'
+                        }
+                    }
+                };
+            }),
+            'get': sinon.spy(function (id) {
+                if (id !== 'id') {
+                    return undefined;
+                }
+                return 'that model'
+            }),
+            pluck: sinon.spy(function () {
+                return [
+                    'orange',
+                    'purple',
+                    'yellow',
+                    'grey',
+                    'green',
+                    'green',
+                    'green',
+                    'red',
+                    'red'
+                ]
+            }),
+            remove: sinon.spy()
+        },
+        endpoint: 'point'
+    };
+};
