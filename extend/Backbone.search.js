e36083a0c5c6f5b01d731c74e527049d1d0205e8
mhofman <mel@capone.nl>
2015-06-01 09:55:25 +0200

    The Backbone search logic is complete. We now have a search that works like the Backbone.Collection.prototype.where method. It's extended so it can look for multiple values and it takes regular expressions for selection.

--- extend/Backbone.search.js
+++ extend/Backbone.search.js
@@ -1,7 +1,7 @@
 var _ = require('underscore');
 var Backbone = require('backbone');
 module.exports = Backbone.Collection.extend({
-    where: function (attrs, first) {
+    search: function (attrs, first) {
         if (_.isEmpty(attrs)) return first ? void 0 : [];
         return this[first ? 'find' : 'filter'](function (model) {
             for (var key in attrs) {
@@ -12,6 +12,11 @@ module.exports = Backbone.Collection.extend({
                     needles = attrs[key];
                 }
                 if (!_.contains(needles, model.get(key))) {
+                    for (var i = 0; i < needles.length; i++) {
+                        if (RegExp('^' + needles[i] + '$').test(model.get(key))) {
+                            return true;
+                        }
+                    }
                     return false;
                 }
             }
---
+++ extend/Backbone.search.spec.js
@@ -0,0 +1,58 @@
+var chai = require('chai');
+var expect = chai.expect;
+var Backbone = require('backbone');
+Backbone.Collection = require('../extend/Backbone.search.js')
+describe('Backbone.search', function () {
+    before(function () {
+        this.model = new Backbone.Model({a: 1});
+        this.coll = new Backbone.Collection([
+            this.model,
+            {a: 1},
+            {a: 1, b: 2},
+            {a: 2, b: 2},
+            {a: 3}
+        ]);
+
+        this.collRegex = new Backbone.Collection([
+            this.model,
+            {a: 'Elephant'},
+            {a: 'Elephant', b: 'Snake'},
+            {a: 'Indian Elephant', b: 'Python'},
+            {a: 'Brown Bear'},
+            {a: 'Grizzly Bear'},
+            {a: 'Polar Bear'}
+        ]);
+
+    });
+    it('passes the same acceptance criteria as the original Backbone.Collection.prototype.where spec', function () {
+        expect(this.coll.search({a: 1}).length).to.equal(3);
+        expect(this.coll.search({a: 2}).length).to.equal(1);
+        expect(this.coll.search({a: 3}).length).to.equal(1);
+        expect(this.coll.search({b: 1}).length).to.equal(0);
+        expect(this.coll.search({b: 2}).length).to.equal(2);
+        expect(this.coll.search({a: 1, b: 2}).length).to.equal(1);
+    });
+    it('can search multiple values for the same key', function () {
+        expect(this.coll.search({a: 1}).length).to.equal(3);
+        expect(this.coll.search({a: [1, 2]}).length).to.equal(4);
+        expect(this.coll.search({a: [2, 3]}).length).to.equal(2);
+    });
+
+    describe('can do regex searches on models in a collection', function () {
+        it('wraps the regexes in a ^$ so only specific searchers get picked up', function () {
+            expect(this.collRegex.search({a: 'Elephant'}).length).to.equal(2);
+            expect(this.collRegex.search({a: 'Elephant', b: 'Python'}).length).to.equal(0);
+            expect(this.collRegex.search({a: 'Elephant', b: 'Snake'}).length).to.equal(1);
+            expect(this.collRegex.search({a: 'Indian Elephant', b: 'Python'}).length).to.equal(1);
+        });
+        it('still does multiple value searches', function () {
+            expect(this.collRegex.search({a: ['Elephant', 'Brown Bear']}).length).to.equal(3);
+        });
+        it('takes a regex to find all Elephant related values', function () {
+            expect(this.collRegex.search({a: ['.*Elephant']}).length).to.equal(3);
+            expect(this.collRegex.search({a: ['(Brown|Grizzly|Polar) Bear']}).length).to.equal(3);
+            expect(this.collRegex.search({a: ['Elephant']}).length).to.equal(2);
+        })
+
+    });
+});
