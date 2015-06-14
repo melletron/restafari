43bcf4835f1076039e93b4b593df3fd5f0e987a5
mhofman <mel@capone.nl>
2015-06-14 20:11:40 +0200

    Starting database connection experiments

---
+++ collections/Tigers.js
@@ -0,0 +1,16 @@
+//var mongoCfg = require('../mongo.config.js');
+//var Mongo = require('mongodb');
+//
+//
+//var TigerInterface = function () {
+//
+//};
+//
+//TigerInterface.prototype = {    };
+//
+//Mongo.MongoClient.connect(mongoCfg.url, function (err, mongodb) {
+//
+//    var collection = mongodb.collection('tigers');
+//
+//
+//});
---
+++ mongo.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "hostname": "127.0.0.1",
+    "host": "127.0.0.1",
+    "port": 27017,
+    "username": "",
+    "password": "",
+    "name": "cf8a061c-ae63-4a6c-8741-f72990785f50",
+    "db": "mPay",
+    "url": "mongodb://localhost:27017/mPay"
+};
